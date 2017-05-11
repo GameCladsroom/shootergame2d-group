fda9573d2252dfc6a026ae0a844e9baf68dadb6a
Bogdan <misterBogdan86@mail.ru>
2017-05-11 09:14:55 +0300

    Add Grandma

--- ShooterGame/src/Grandma.js
+++ ShooterGame/src/Grandma.js
@@ -1,8 +1,35 @@
 
 var Grandma = cc.Sprite.extend({
-    ctor: function ()
-    {
-        this._super();
-        
-    }
+    direction:0,
+    ctor: function () {
+        this._super(res.GrandmaImage);
+
+        var me = this;
+        var keyboard_listener = cc.EventListener.create({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed: function (keyCode, event) {
+                if (keyCode == cc.KEY.up) {
+                    me.direction = 1;
+                }
+                if (keyCode == cc.KEY.down) {
+                    me.direction = -1;
+                }
+            },
+            onKeyReleased: function (keyCode, event) {
+                me.direction = 0;
+            }
+        });
+        cc.eventManager.addListener(keyboard_listener, this);
+
+        this.scheduleUpdate();
+        return true;
+
+    },
+
+    update:function (dt) {
+
+        this.y+=this.direction;
+
+    },
+
 });
