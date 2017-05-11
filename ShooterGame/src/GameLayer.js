8212b7aebe7a8f14c4c13560af55a97f2c3cbca9
EmiratliArt <artur.emiratli@mail.ru>
2017-05-11 09:00:35 +0400

    add Colizion

--- ShooterGame/src/GameLayer.js
+++ ShooterGame/src/GameLayer.js
@@ -15,6 +15,48 @@ var GameLayer = cc.Layer.extend({
         this.addChild(this.zombiesLayer);
 
         return true;
+    },
+    update: function (dt) {
+        if (this.zombiesLayer.zombie!= null) {
+            var zombox = this.zombiesLayer.zombie.getBoundingBox();
+            if (zombox.x + zombox.width <= 100){
+                this.removeZomb();
+            }
+        }
+
+
+
+        if (this.grandma != null && this.zombiesLayer.zombie!= null) {
+
+            var box = this.grandma.getBoundingBox();
+            var box1 = this.zombiesLayer.zombie.getBoundingBox();
+            if (cc.rectIntersectsRect(box, box1)) {
+                this.grandma.removeFromParent();
+                this.grandma = null;
+                var helloLabel = new cc.LabelTTF("Game over!", "Calibi", 38);
+
+                helloLabel.x = cc.winSize.width / 2;
+                helloLabel.y = cc.winSize.height / 2;
+                helloLabel.color = cc.color(000, 0, 0);
+
+                this.addChild(helloLabel, 5);
+            }
+        }
+    },
+    removeZomb: function(){
+        this.zombiesLayer.zombie.removeFromParent();
+        this.zombiesLayer.zombie = null;
     }
+
+
+
+
+
+
+
+
+
+
+
 });
 
